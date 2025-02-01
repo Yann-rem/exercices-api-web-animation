9f604e992b1cee60256b52969a5f8d37ca4f413c
Yannick Remy <remyyannick@sfr.fr>
2025-02-01 10:16:36 +0100

    feat: ajout de l'animation des boîtes vertes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Sélection des éléments HTML
 const redBox = document.querySelector(".container1 .box");
+const greenBox1 = document.querySelector(".container2 .box1");
+const greenBox2 = document.querySelector(".container2 .box2");
 const button1 = document.querySelector(".btn1");
+const button2 = document.querySelector(".btn2");
 
 // Variable de contrôle pour la direction de l'animation
 let isRedBoxAtStart = false;
+let isGreenBoxAtStart = false;
 
 // Fonction pour créer une animation
 const createAnimation = (element, keyframes, options) => {
@@ -30,4 +34,29 @@ button1.addEventListener("click", async () => {
   }
 
   isRedBoxAtStart = !isRedBoxAtStart;
+});
+
+// Animation des boîtes vertes
+const greenBox1Keyframes = [{ transform: "translateX(450px)" }];
+const greenBox2Keyframes = [{ transform: "translateX(-450px)" }];
+const greenBoxOptions = { duration: 1000, fill: "forwards", easing: "ease-in-out" };
+
+const greenBoxesAnimation = [
+  createAnimation(greenBox1, greenBox1Keyframes, greenBoxOptions),
+  createAnimation(greenBox2, greenBox2Keyframes, greenBoxOptions),
+];
+
+button2.addEventListener("click", () => {
+  const direction = isGreenBoxAtStart ? -1 : 1;
+
+  greenBoxesAnimation.forEach((animation) => {
+    if (animation.playState === "running") {
+      animation.playbackRate = direction;
+    } else {
+      animation.playbackRate = direction;
+      animation.play();
+    }
+  });
+
+  isGreenBoxAtStart = !isGreenBoxAtStart;
 });
